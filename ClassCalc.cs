eb3c0251f5ad1a11bea5c1622c4486edf67b12cd
Snezhana98 <snezhank67@gmail.com>
2023-01-08 14:37:56 +0400

    Добавьте файлы проекта.

---
+++ ClassCalc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+enum Znaki { plus, minus, mul, div};
+
+namespace Calculator
+{
+    public class ClassCalc
+    {
+        int FirstDigit;
+        int SecondDigit;
+        string Znaki;
+       
+    }
+}
