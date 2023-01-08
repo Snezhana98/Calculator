92353635479769ab88556af0619d1a365ee5de79
Snezhana98 <snezhank67@gmail.com>
2023-01-08 16:48:14 +0400

    редактирование

--- ClassCalc.cs
+++ ClassCalc.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-enum Znaki { plus, minus, mul, div};
+
 
 namespace Calculator
 {
+    public enum Znaki { none, plus, minus, mul, div };
     public class ClassCalc
     {
-        int FirstDigit;
-        int SecondDigit;
-        string Znaki;
-       
+        public int FirstDigit;
+        public int SecondDigit;
+        public string ZnakString;
+        public string k;
+        public int proverka = 1;
+        //public Znaki res() { 
+
+        //    switch (ZnakString)
+        //    {
+        //        case "plus":
+        //            return FirstDigit + SecondDigit;
+
+        //        case "minus":
+        //            return FirstDigit - SecondDigit;
+
+        //        case "mul":
+        //            return FirstDigit * SecondDigit;
+
+        //        case "div":
+        //            return FirstDigit / SecondDigit;
+        //    }
+        //}
+
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,8 +18,10 @@ namespace Calculator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        ClassCalc Vivod;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,22 +29,74 @@ namespace Calculator
 
         private void ClickChislo(object sender, RoutedEventArgs e)
         {
-            sender as new ClassCalc.FirstDigit; 
+            if (sender is Button btn)
+            {
+                if (Vivod == null)
+                    Vivod = new ClassCalc();
+                int n = int.Parse(btn.Content.ToString());
+                VvodPrimera.Text = n.ToString();
+
+                if (Vivod.ZnakString == null)
+
+                    Vivod.FirstDigit = n;
+                else
+                {
+                    Vivod.SecondDigit = n;
+                    Vivod.proverka = n;
+                }
+            }
         }
 
         private void ClickSbros(object sender, RoutedEventArgs e)
         {
-
+            VvodPrimera.Text = "0";
+            VerhVivodPrimera.Text = "";
+            Vivod = null;
         }
 
         private void ClickRavno(object sender, RoutedEventArgs e)
         {
-
+            if (sender is Button btn)
+            {
+                if (Vivod.ZnakString == "div" && Vivod.SecondDigit == 0 && Vivod.proverka == 0)
+                {
+                    VvodPrimera.Text = "Деление на 0";
+                    Vivod.proverka = 1;
+                }
+                else
+                {
+                    VerhVivodPrimera.Text = Vivod.SecondDigit + " " + Vivod.k + " " + Vivod.FirstDigit;
+                    //VvodPrimera.Text = Vivod.res().ToString();
+                }
+            }
         }
 
         private void ClickZnak(object sender, RoutedEventArgs e)
         {
-
+            if (sender is Button btn)
+            {
+                Vivod.ZnakString = btn.Content.ToString();
+                Vivod.k = Vivod.ZnakString;
+                VerhVivodPrimera.Text = Vivod.k + " " + Vivod.FirstDigit;
+                
+               
+                    switch (Vivod.ZnakString)
+                    {
+                        case "+":
+                            Vivod.ZnakString = "plus";
+                            break;
+                        case "-":
+                            Vivod.ZnakString = "minus";
+                            break;
+                        case "*":
+                            Vivod.ZnakString = "mul";
+                            break;
+                        case "/":
+                            Vivod.ZnakString = "div";
+                            break;
+                    
+                }
+            }
         }
     }
 }
